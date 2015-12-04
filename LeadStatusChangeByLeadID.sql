9284ae667ccca4872dddaac851100731a06064a8
Wahid Alimi <wahid.alimi@frontpointsecurity.com>
2015-12-04 17:07:38 -0500

    Further refined the query.

--- LeadStatusChangeByLeadID.sql
+++ LeadStatusChangeByLeadID.sql
@@ -9,12 +9,12 @@ where c.Status is not null
 and a.LeadID in (
 	select LeadID
 	from LeadStatusChange 
-	where DateCreated > '2015-11-01'
+	where DateCreated > '2015-01-01'
 	group by LeadID
-	having count(LeadID) >= 1
+	having count(LeadID) > 1
 	)
 group by a.LeadID, c.Status, b.DateCreated
-order by DateCreated
+order by LeadID, DateCreated
 
 
 select * from LeadStatusChange
